f1beec16382287f693dcf20807198ce0f2a56fa9
Jason Necaise <jnecaise@dwavesys.com>
2022-01-29 15:42:23 -0800

    upload

--- maxcut/qiskit/maxcut_benchmark.py
+++ maxcut/qiskit/maxcut_benchmark.py
@@ -7,6 +7,7 @@ import time
 from collections import namedtuple
 
 import numpy as np
+from scipy.optimize import minimize
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import Aer, execute # for computing expectation tables
 
@@ -60,41 +61,32 @@ def create_qaoa_circ(nqubits, edges, parameters):
     return qc
     
 # Create the benchmark program 
-def MaxCut (num_qubits, secret_int, edges, method = 1, rounds = 1):
+def MaxCut (num_qubits, secret_int, edges, method = 1, rounds = 1, theta=None,):
     
-    # Method 1 - execute one instance of ansatz and determine fidelity
-    if method == 1:
+    # IF angles dict are given, then bypass method check;
+    # just retern the quantum circuit with those angles
     
-        # create ansatz circuit
+    if theta is None:
+        if method == 1:
+            # Set default angles
+            theta = 2*rounds*[1.0]
         
-        theta = 2*rounds*[1.0]
+    # put parameters into the form expected by the ansatz generator
+    p = len(theta)//2  # number of qaoa rounds
+    beta = theta[:p]
+    gamma = theta[p:]
+    parameters = [QAOA_Parameter(*t) for t in zip(beta,gamma)]
         
-        # put parameters into the form expected by the ansatz generator
-        p = len(theta)//2  # number of qaoa rounds
-        beta = theta[:p]
-        gamma = theta[p:]
-        parameters = [QAOA_Parameter(*t) for t in zip(beta,gamma)]
-        
-        # and create the circuit, without measurements
-        qc = create_qaoa_circ(num_qubits, edges, parameters)   
+    # and create the circuit, without measurements
+    qc = create_qaoa_circ(num_qubits, edges, parameters)   
 
-        # pre-compute and save an array of expected measurements
-        compute_expectation(qc, num_qubits, secret_int)
+    # pre-compute and save an array of expected measurements
+    compute_expectation(qc, num_qubits, secret_int)
         
-        # add the measure here
-        qc.measure_all()
+    # add the measure here
+    qc.measure_all()
         
-       
-    # Method 2 - execute multiple instances of ansatz, primarily to measure execution time average
-    # DEVNOTE: unclear if this is the desired approach yet
-    elif method == 2:
-        pass
-    
-    # Method 3 - include classical minimizer to control execution and to obtain optimal result
-    # Need to build in some sophistication here to control for time/quality profile determination - TBD
-    elif method == 3:
-        pass
-        
+
     # save small circuit example for display
     global QC_
     if QC_ == None or num_qubits <= 6:
@@ -115,12 +107,10 @@ expectations = {}
 
 # Compute array of expectation values in range 0.0 to 1.0
 # Use statevector_simulator to obtain exact expectation
-def compute_expectation(qc, num_qubits, secret_int):
-    
-    #ts = time.time()
+def compute_expectation(qc, num_qubits, secret_int, backend_id='statevector_simulator'):
     
     #execute statevector simulation
-    sv_backend = Aer.get_backend('statevector_simulator')
+    sv_backend = Aer.get_backend(backend_id)
     sv_result = execute(qc, sv_backend).result()
 
     # get the probability distribution
@@ -175,18 +165,35 @@ def analyze_and_print_result (qc, result, num_qubits, secret_int, num_shots):
     
     return counts, fidelity
 
+# Compute the objective function on a given sample
+def compute_objective(results, nodes, edges):
+    counts = results.get_counts()
+    
+    avg = 0
+    sum_count = 0
+    for solution, count in counts.items():
+        obj = -1*common.eval_cut(nodes, edges, solution)
+
+        avg += obj * count
+        sum_count += count
+
+    return avg/sum_count
+
 
 ################ Benchmark Loop
 
 # Problem definitions only available for up to 10 qubits currently
 MAX_QUBITS = 10
+saved_result = None
+instance_filename = None
 
 # Execute program with default parameters
-def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=10000,
+def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=100,
         method=1, rounds=1,
         backend_id='qasm_simulator', provider_backend=None,
         hub="ibm-q", group="open", project="main", exec_options=None):
-
+    global circuits_done
+    
     print("MaxCut Benchmark Program - Qiskit")
 
     # validate parameters (smallest circuit is 4 qubits)
@@ -207,8 +214,25 @@ def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=10000,
         counts, fidelity = analyze_and_print_result(qc, result, num_qubits, int(s_int), num_shots)
         metrics.store_metric(num_qubits, s_int, 'fidelity', fidelity)
 
+    def execution_handler2 (qc, result, num_qubits, s_int, num_shots):
+        global saved_result
+        global instance_filename
+        
+        nodes, edges = common.read_maxcut_instance(instance_filename)
+        opt, _ = common.read_maxcut_solution(instance_filename)
+        
+        f = -1 * compute_objective(result, nodes, edges) / opt
+        metrics.store_metric(num_qubits, s_int, 'fidelity', f)
+        
+        saved_result = result
+     
     # Initialize execution module using the execution result handler above and specified backend_id
-    ex.init_execution(execution_handler)
+    if method == 2:
+        ex.max_shots = 1
+        ex.init_execution(execution_handler2)
+    else:
+        ex.init_execution(execution_handler)
+    
     ex.set_execution_target(backend_id, provider_backend=provider_backend,
             hub=hub, group=group, project=project, exec_options=exec_options)
 
@@ -218,48 +242,98 @@ def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=10000,
     # Execute Benchmark Program N times for multiple circuit sizes
     # Accumulate metrics asynchronously as circuits complete
     # DEVNOTE: increment by 2 to match the collection of problems in 'instance' folder
-    for num_qubits in range(min_qubits, max_qubits + 1, 2):
+    for p_depth in [2, 4]:
+        for num_qubits in range(min_qubits, max_qubits + 1, 2):
             
-        # determine number of circuits to execute for this group
-        #num_circuits = min(2**(num_qubits), max_circuits)
-        num_circuits = max_circuits
+            # determine number of circuits to execute for this group
+            #num_circuits = min(2**(num_qubits), max_circuits)
+            num_circuits = max_circuits
         
-        print(f"************\nExecuting [{num_circuits}] circuits with num_qubits = {num_qubits}")
+            print(f"************\nExecuting [{num_circuits}] circuits with num_qubits = {num_qubits}")
           
-        # loop over limited # of inputs for this
-        # assume the solution files start with 3 and go up from there
-        circuits_done = 0
-        for i in range(3, 3 + max_circuits):
+            # loop over limited # of inputs for this
+            # assume the solution files start with 3 and go up from there
+            circuits_done = 0
+            for i in range(3, 3 + max_circuits):
             
-            # create integer that represents the problem instance; use s_int as circuit id
-            s_int = i
-            #print(f"  ... i={i} s_int={s_int}")
             
-            # create filename from num_qubits and circuit_id (s_int), then load the problem file
-            instance_filename = f"instance/mc_{str(num_qubits).zfill(3)}_{str(i).zfill(3)}_000.txt"
-            #print(f"... instance_filename = {instance_filename}")
-            nodes, edges = common.read_maxcut_instance(instance_filename)
-            #print(f"nodes = {nodes}")
-            #print(f"edges = {edges}")
             
-            # if the file does not exist, we are done with this number of qubits
-            if nodes == None:
-                print(f"  ... problem {str(i).zfill(3)} not found, limiting to {circuits_done} circuit(s).")
-                break;
+                # create integer that represents the problem instance; use s_int as circuit id
+                s_int = i
+                #print(f"  ... i={i} s_int={s_int}")
             
-            circuits_done += 1
+                # create filename from num_qubits and circuit_id (s_int), then load the problem file
+                global instance_filename
+                instance_filename = f"instance/mc_{str(num_qubits).zfill(3)}_{str(i).zfill(3)}_000.txt"
+                #print(f"... instance_filename = {instance_filename}")
+                nodes, edges = common.read_maxcut_instance(instance_filename)
+                #print(f"nodes = {nodes}")
+                #print(f"edges = {edges}")
             
-            # create the circuit for given qubit size and secret string, store time metric
-            ts = time.time()
-            qc = MaxCut(num_qubits, s_int, edges, method, rounds)
-            metrics.store_metric(num_qubits, s_int, 'create_time', time.time()-ts)
+                # if the file does not exist, we are done with this number of qubits
+                if nodes == None:
+                    print(f"  ... problem {str(i).zfill(3)} not found, limiting to {circuits_done} circuit(s).")
+                    break;
+            
+            
+                if method != 2:
+            
+                    circuits_done += 1
+            
+                    # create the circuit for given qubit size and secret string, store time metric
+                    ts = time.time()
+                    qc = MaxCut(num_qubits, s_int, edges, method, rounds)
+                    metrics.store_metric(num_qubits, s_int, 'create_time', time.time()-ts)
 
-            # collapse the sub-circuit levels used in this benchmark (for qiskit)
-            qc2 = qc.decompose()
+                    # collapse the sub-circuit levels used in this benchmark (for qiskit)
+                    qc2 = qc.decompose()
 
-            # submit circuit for execution on target (simulator, cloud simulator, or hardware)
-            ex.submit_circuit(qc2, num_qubits, s_int, shots=num_shots)
-        
+                    # submit circuit for execution on target (simulator, cloud simulator, or hardware)
+                    ex.submit_circuit(qc2, num_qubits, s_int, shots=num_shots)
+                
+            
+                if method == 2:
+                
+                    thetas_init = 2*p_depth*[1.0]
+                
+                    def expectation(theta):
+                        global circuits_done
+                    
+                        circuits_done += 1
+                    
+                        # Every circuit needs a unique id; add circuits_done instead of s_int
+                        unique_id = s_int*1000 + circuits_done
+                    
+                        # create the circuit for given qubit size and secret string, store time metric
+                        ts = time.time()
+                        qc = MaxCut(num_qubits, unique_id, edges, method, rounds, theta)
+                        metrics.store_metric(num_qubits, unique_id, 'create_time', time.time()-ts)
+
+                        # collapse the sub-circuit levels used in this benchmark (for qiskit)
+                        qc2 = qc.decompose()
+
+                        # submit circuit for execution on target (simulator, cloud simulator, or hardware)
+                        ex.submit_circuit(qc2, num_qubits, unique_id, shots=num_shots)
+                    
+                        # Must wait for circuit to complete
+                        #ex.throttle_execution(metrics.finalize_group)
+                        ex.finalize_execution(metrics.finalize_group)
+                    
+                        return compute_objective(saved_result, nodes, edges)
+                
+                    res = minimize(expectation, thetas_init, method='COBYLA')
+                    opt, sol = common.read_maxcut_solution(instance_filename)
+                
+                    num_qubits = int(num_qubits)
+                    #counts, fidelity = analyze_and_print_result(qc, result, num_qubits, int(s_int), num_shots)
+                    fidelity = -1 * res.fun / opt #known optimum
+                
+                    metrics.store_metric(num_qubits, s_int, 'fidelity', fidelity)
+                    metrics.store_metric(num_qubits, s_int, 'rounds', p_depth)
+                
+                    #print(res)
+                
+                
         # Wait for some active circuits to complete; report metrics when groups complete
         ex.throttle_execution(metrics.finalize_group)
         
@@ -274,5 +348,5 @@ def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=10000,
     metrics.plot_metrics(f"Benchmark Results - MaxCut ({method}) - Qiskit")
 
 # if main, execute method
-if __name__ == '__main__': run()
+if __name__ == '__main__': run(method=2)
    
